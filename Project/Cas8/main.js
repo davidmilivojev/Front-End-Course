b194408d6c749ec32899a8ffb779681ed61f363d
davidmilivojev <davidmilivojev@gmail.com>
2021-03-02 18:29:02 +0100

    cas8

--- Project/Cas8/main.js
+++ Project/Cas8/main.js
@@ -73,6 +73,32 @@ function cookieStrg() {
         }
     });
 }
-localStrg();
+// localStrg();
 // sessionStrg();
 // cookieStrg();
+
+function dropdown() {
+    const $btn = $('.js-drop-btn');
+    const $liBtn = $('.js-li-btn');
+    const active = 'dropdown-btn-active';
+
+    $btn.on('click', function() {
+        $(this).toggleClass(active);
+        $(this).next().stop().slideToggle();
+    });
+
+    $liBtn.on('click', function() {
+        const text = $(this).text();
+        $btn.text(text).removeClass(active);
+        $(this).closest('.js-drop-list').stop().slideUp();
+    });
+
+    $(document).on('click', function(e) {
+        if(!$(e.target).closest('.dropdown').length) {
+            $btn.removeClass(active);
+            $('.js-drop-list').stop().slideUp();
+        }
+    });
+}
+
+dropdown();
