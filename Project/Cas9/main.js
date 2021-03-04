a80894543a52acc44f824a900b4432f319b2cb4e
davidmilivojev <davidmilivojev@gmail.com>
2021-03-04 18:29:35 +0100

    cas 9 plugins

--- Project/Cas9/main.js
+++ Project/Cas9/main.js
@@ -1,104 +1,36 @@
-const $btn = $('.btn');
-const rem = 'rem';
-const add = 'set';
-const get = 'get';
-
-function localStrg() {
-
-    $btn.on('click', function() {
-        if($(this).hasClass(add)) {
-            localStorage.setItem('key', 'david');
-        } else if($(this).hasClass(get)) {
-            const getValue = localStorage.getItem('key');
-            $('.text').text(getValue);
-        } else if($(this).hasClass(rem)) {
-            localStorage.removeItem('key');
-            $('.text').empty();
-        } else {
-            localStorage.clear();
-            $('.text').empty();
-        }
-    });
-}
-
-function sessionStrg() {
-    $btn.on('click', function() {
-        if($(this).hasClass(add)) {
-            sessionStorage.setItem('key', 'david');
-        } else if($(this).hasClass(get)) {
-            const getValue = sessionStorage.getItem('key');
-            $('.text').text(getValue);
-        } else if($(this).hasClass(rem)) {
-            sessionStorage.removeItem('key');
-            $('.text').empty();
-        } else {
-            sessionStorage.clear();
-            $('.text').empty();
-        }
-    });
-
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+function cardHeight() {
+    const option = {
+        byRow: true,
+        property: 'min-height'
     }
-    return "";
-  }
+    $('.js-card').matchHeight(option);
+    $('.card-title').matchHeight();
+}
 
+function slickSlider() {
+    $('.js-slider').slick();
+}
 
-function cookieStrg() {
-    $btn.on('click', function() {
-        if($(this).hasClass(add)) {
-            setCookie('keyTest','value', 3);
-        } else if($(this).hasClass(get)) {
-            const getData = getCookie('keyTest');
-            $('.text').text(getData);
+function swiperSlider() {
+    const swiper = new Swiper('.js-swiper', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        mousewheel: {
+            invert: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
         }
     });
 }
-// localStrg();
-// sessionStrg();
-// cookieStrg();
 
 function dropdown() {
-    const $btn = $('.js-drop-btn');
-    const $liBtn = $('.js-li-btn');
-    const active = 'dropdown-btn-active';
-
-    $btn.on('click', function() {
-        $(this).toggleClass(active);
-        $(this).next().stop().slideToggle();
-    });
-
-    $liBtn.on('click', function() {
-        const text = $(this).text();
-        $btn.text(text).removeClass(active);
-        $(this).closest('.js-drop-list').stop().slideUp();
-    });
-
-    $(document).on('click', function(e) {
-        if(!$(e.target).closest('.dropdown').length) {
-            $btn.removeClass(active);
-            $('.js-drop-list').stop().slideUp();
-        }
-    });
+    $('select').selectric();
 }
-
 dropdown();
+// swiperSlider();
+// slickSlider();
+// cardHeight();
